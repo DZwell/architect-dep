5745a1fa2ac347dd3f432dadef1ebb34d3645fda
Mike Skalandunas <mskalandunas@gmail.com>
2016-09-25 00:27:24 -0700

    add navbar

--- app/index.js
+++ app/index.js
@@ -8,10 +8,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
+import Navbar from './js/components/material-navbar';
 import Footer from './js/components/material-footer';
 
 const Main = () => (
-  <Footer/>
+  <main>
+    <Navbar/>
+    <Footer/>
+  </main>
 );
 
 ReactDOM.render(<Main/>, document.querySelector('#root'));
---
+++ app/js/components/material-hero.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+const Hero = React.createClass({
+  render : function() {
+    return (
+
+    )
+  }
+});
+
+export default Hero;
