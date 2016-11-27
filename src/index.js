730f4bc411c930ddf223b2447d1543361781f2bb
Mike Skalandunas <mskalandunas@gmail.com>
2016-11-26 23:18:46 -0800

    new title

---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import './css/style.css';
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Router, Route, IndexRoute, browserHistory } from 'react-router';
