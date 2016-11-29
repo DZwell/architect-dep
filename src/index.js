326a4f928810252f87986f49210e764a77d1db83
Mike Skalandunas <mskalandunas@gmail.com>
2016-11-29 09:42:49 -0800

    add description prop

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const PlayerContainer = () => (
   <div>
     <div>
       <AudioPlayer
+        desc="This is the basic player. This is your world, whatever makes you happy you can put in it. Go crazy. It's almost like something out of a fairytale book. Let's put some happy trees and bushes back in here. Brown is such a nice color. Clouds are free. They just float around the sky all day and have fun. Just pretend you are a whisper floating across a mountain."
         source="audio/1.wav"
       />
     </div>
--- src/js/components/basic-player.js
+++ src/js/components/basic-player.js
@@ -6,6 +6,7 @@ import _util_ from '../lib/utilities';
 
 const Mosaic = React.createClass({
   propTypes: {
+    desc            : React.PropTypes.string.isRequired,
     source          : React.PropTypes.string.isRequired
   },
 
@@ -19,7 +20,7 @@ const Mosaic = React.createClass({
   sourceDuration    : '',
 
   componentDidMount : function() {
-    const that         = ReactDOM.findDOMNode(this).children[0].children[0];
+    const that         = ReactDOM.findDOMNode(this).children[0].children[0].children[0];
     this.audioNode     = that.children[0];
     this.duration      = that.children[3];
     this.hover         = that.children[2].children[0];
@@ -141,29 +142,32 @@ const Mosaic = React.createClass({
 
   render : function() {
     return (
-      <div className="mosaic-player">
-        <div className="mosaic-type-single">
-          <div className="mosaic-gui mosaic-interface mosaic-player">
-            <audio id={_util_.newId('audio-')} preload="auto" onDurationChange={this.returnDuration} onTimeUpdate={this.updateTime} loop={this.props.loop}>
-              <source src={this.props.source}/>
-            </audio>
-            <ul className="mosaic-controls">
-              <li className="play-button-container">
-                <a className="mosaic-play" onClick={this.play}>
-                  <i className="fa fa-play"></i>
-                </a>
-              </li>
-            </ul>
-            <div className="mosaic-progress" onMouseDown={this.mouseDown}>
-              <div className="mosaic-seek-bar">
-                <div className="mosaic-play-bar" onMouseDown={this.mouseDown}></div>
+      <div>
+        <div className="mosaic-player">
+          <div className="mosaic-type-single">
+            <div className="mosaic-gui mosaic-interface mosaic-player">
+              <audio id={_util_.newId('audio-')} preload="auto" onDurationChange={this.returnDuration} onTimeUpdate={this.updateTime} loop={this.props.loop}>
+                <source src={this.props.source}/>
+              </audio>
+              <ul className="mosaic-controls">
+                <li className="play-button-container">
+                  <a className="mosaic-play" onClick={this.play}>
+                    <i className="fa fa-play"></i>
+                  </a>
+                </li>
+              </ul>
+              <div className="mosaic-progress" onMouseDown={this.mouseDown}>
+                <div className="mosaic-seek-bar">
+                  <div className="mosaic-play-bar" onMouseDown={this.mouseDown}></div>
+                </div>
               </div>
-            </div>
-            <div className="mosaic-time-holder">
-              <span></span>
+              <div className="mosaic-time-holder">
+                <span></span>
+              </div>
             </div>
           </div>
         </div>
+        <p>{this.props.desc}</p>
       </div>
     )
   }
