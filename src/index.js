9559bdbf295c45fc5d8d23c37449e5a7d7996b08
Mike Skalandunas <mskalandunas@gmail.com>
2016-11-29 16:02:01 -0800

    add example data

--- src/index.js
+++ src/index.js
@@ -8,13 +8,14 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import AudioPlayer from './js/components/basic-player';
+import examples from './js/lib/example-data';
 
 const PlayerContainer = () => (
   <div>
     <div>
       <AudioPlayer
-        desc="This is the basic player. This is your world, whatever makes you happy you can put in it. Go crazy. It's almost like something out of a fairytale book. Let's put some happy trees and bushes back in here. Brown is such a nice color. Clouds are free. They just float around the sky all day and have fun. Just pretend you are a whisper floating across a mountain."
-        source="audio/1.wav"
+        desc={examples[0].desc}
+        source={examples[0].src}
       />
     </div>
   </div>
---
+++ src/js/lib/example-data.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const examples = [
+  {
+    'desc' : 'This is the basic player.  It provides non-looped playback of whatever track the user supplis.  This is best used for previewing title, credit, or any other track that may not need to be heard over and over.',
+    'src' : 'audio/1.wav'
+  }
+];
+
+export default examples;
